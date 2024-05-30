3591a06b3d35e6a01ed2ad29ca170564439ab42a
101 <tmddnjs001133@dsm.hs.kr>
2024-05-30 19:23:07 +0900

    logger, coroutine 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+    implementation("org.slf4j:slf4j-api:1.7.36")
+    implementation("ch.qos.logback:logback-classic:1.2.10")
+
 }
 
 tasks.test {
